70e39f93f579564adb9498dc67344a660ed1a64c
chrisbenedettini <cbttini@gmail.com>
2024-10-30 22:51:42 -0500

    update to counter

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -5,6 +5,7 @@ import { Creepster } from "next/font/google";
 import CounterTitle from "./CounterTitle";
 import CounterImage from "./CounterImage";
 import dayjs from "dayjs";
+import { Span } from "next/dist/trace";
 
 const creepster = Creepster({
   subsets: ["latin"],
@@ -37,38 +38,60 @@ function Counter() {
         <div className="flex flex-1 flex-col items-center text-shadow-black mt-8 md:mt-10 xl:pr-10 xl:mt-0  ">
           <CounterTitle time={daysUntil} />
           <div
-            className={` ${creepster.variable} font-sans grid bg-black/50 rounded-3xl grid-flow-col gap-3 text-center auto-cols-max mt-4 p-6 sm:gap-5 sm:p-8 lg:gap-7 xl:p-12 `}
+            className={`${creepster.variable} font-sans grid bg-black/50 rounded-3xl grid-flow-col gap-3 text-center auto-cols-max mt-4 p-6 sm:gap-5 sm:p-8 lg:gap-7 xl:p-12`}
           >
             <div className="flex flex-col text-white text-3xl sm:text-4xl lg:text-5xl xl:text-6xl">
-              <span className="countdown text-7xl sm:text-8xl xl:text-9xl">
-                <span
-                  style={{ "--value": `${d}` } as React.CSSProperties}
-                ></span>
-              </span>
+              {d === 365 ? (
+                <span className="text-7xl sm:text-8xl xl:text-9xl">00</span>
+              ) : (
+                <span className="countdown text-7xl sm:text-8xl xl:text-9xl">
+                  <span
+                    style={{ "--value": `${d}` } as React.CSSProperties}
+                  ></span>
+                </span>
+              )}
               days
             </div>
             <div className="flex flex-col text-white text-3xl sm:text-4xl lg:text-5xl xl:text-6xl ">
-              <span className="countdown  text-7xl sm:text-8xl xl:text-9xl">
-                <span
-                  style={{ "--value": `${h}` } as React.CSSProperties}
-                ></span>
-              </span>
+              {d === 365 ? (
+                <span className="countdown text-7xl sm:text-8xl xl:text-9xl">
+                  00
+                </span>
+              ) : (
+                <span className="countdown  text-7xl sm:text-8xl xl:text-9xl">
+                  <span
+                    style={{ "--value": `${h}` } as React.CSSProperties}
+                  ></span>
+                </span>
+              )}
               hours
             </div>
             <div className="flex flex-col text-white text-3xl sm:text-4xl lg:text-5xl xl:text-6xl">
-              <span className="countdown  text-7xl sm:text-8xl xl:text-9xl">
-                <span
-                  style={{ "--value": `${m}` } as React.CSSProperties}
-                ></span>
-              </span>
+              {d === 365 ? (
+                <span className="countdown text-7xl sm:text-8xl xl:text-9xl">
+                  00
+                </span>
+              ) : (
+                <span className="countdown  text-7xl sm:text-8xl xl:text-9xl">
+                  <span
+                    style={{ "--value": `${m}` } as React.CSSProperties}
+                  ></span>
+                </span>
+              )}
               min
             </div>
             <div className="flex flex-col text-white text-3xl sm:text-4xl lg:text-5xl xl:text-6xl">
-              <span className="countdown   text-7xl sm:text-8xl xl:text-9xl">
-                <span
-                  style={{ "--value": `${s}` } as React.CSSProperties}
-                ></span>
-              </span>
+              {d === 365 ? (
+                <span className="countdown text-7xl sm:text-8xl xl:text-9xl">
+                  00
+                </span>
+              ) : (
+                <span className="countdown   text-7xl sm:text-8xl xl:text-9xl">
+                  <span
+                    style={{ "--value": `${s}` } as React.CSSProperties}
+                  ></span>
+                </span>
+              )}
               sec
             </div>
           </div>
